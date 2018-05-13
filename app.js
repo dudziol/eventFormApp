1374ce2af107da5dc0e8474236a939db2e7a0421
dudzio <dud2iol@gmail.com>
2018-05-13 15:22:48 +0200

    app config added

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 var express = require("express"),
-	app = express();
+	app = express(),
+	mongoose = require("mongoose"),
+	bodyParser = require("body-parser");
 
+
+// APP CONFIG
+mongoose.connect("mongodb://localhost/eventForm")
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+
+// ROUTES
 app.get("/", function(req, res){
-	res.send("Hello")
-})
+	res.render("index");
+});
 
 app.listen(3000, function(){
 	console.log("Server running on port 3000");
-})
+});

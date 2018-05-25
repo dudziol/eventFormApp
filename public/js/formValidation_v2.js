593e6b5a116b24c8feddd55863ec66b5a547a4dd
dudzio <dud2iol@gmail.com>
2018-05-25 21:30:17 +0200

    front validation refactor

---
+++ public/js/formValidation_v2.js
@@ -0,0 +1,91 @@
+const errorMessages = {
+	missingValueError: "This field is required",
+	nameError: "You can use only: A-Z, a-z",
+	emailError: "You have to follow pattern: xxx@xxx.xx"
+}
+
+function createErrorMessage(id, message){
+	return document.getElementById(id + "Message").innerHTML = message
+}
+
+function validateFormHelper(id, message){
+	if(id === "name"){
+		createErrorMessage(id, message.nameError);
+	}else if(id === "email"){
+		createErrorMessage(id, message.emailError);
+	} else{
+		createErrorMessage(id, message.missingValueError);
+	}
+}
+
+function Form(form, errorMessages){
+	this.errorMessages = errorMessages;
+	this.isFormValid = false;
+	this.removeFields = function(){
+		this.fields = [];
+	}
+	this.createFields = function(){
+		for (let i=0; i<form.length; i++){
+			this.fields.push(new Field(form[i].id, form[i].value, form[i].pattern));
+		}
+	}
+	this.validateForm = function(){
+		for(let i=0; i<this.fields.length;i++){
+			this.fields[i].validateField();
+			if(!this.fields[i].isFieldValid){
+				if(this.fields[i].isEmpty){
+				createErrorMessage(this.fields[i].id, this.errorMessages.missingValueError);
+				} else{
+					validateFormHelper(this.fields[i].id, this.errorMessages);
+				}
+			} else{
+				createErrorMessage(this.fields[i].id, "");		
+			}
+		}
+		for(let i=0; i<this.fields.length; i++){
+			if(!this.fields[i].isFieldValid){
+				this.isFormValid = false;
+				break;
+			} else{
+				this.isFormValid = true;
+			}
+		}
+	}
+}
+
+function Field(id, value, pattern){
+	this.id = id;
+	this.value = value;
+	this.pattern = new RegExp("^"+pattern+"$");
+	this.isFieldValid = false;
+	this.isEmpty = true;
+	this.isPatternValid = false;
+	this.validateField = function(){
+		if(this.value === ""){
+			this.isEmpty = true;
+		} else{
+			this.isEmpty = false;
+		}
+		if(this.pattern.test(this.value)){
+			this.isPatternValid = true;
+		} else{
+			this.isPatternValid = false;
+		}
+		if((this.isEmpty === false)&&(this.isPatternValid === true)){
+			this.isFieldValid = true;
+		} else{
+			this.isFieldValid = false;
+		}
+	}
+}
+
+var Form = new Form(document.querySelectorAll("div > input"), errorMessages);
+document.getElementById("submit").addEventListener("click", function(event){
+	Form.removeFields();
+	Form.createFields();
+	Form.validateForm();
+	if(!Form.isFormValid){
+		event.preventDefault();
+	}
+});
+
